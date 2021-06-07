82585d47c3519be13f0150e8c45df9b36982d5e6
admin <admin@DESKTOP-OLVM896>
2021-06-07 17:16:04 +0900

    매칭 게시판 오류 수정

--- src/main/java/dogwalk/service/MBWrite.java
+++ src/main/java/dogwalk/service/MBWrite.java
@@ -1,5 +1,7 @@
 package dogwalk.service;
 
+import java.sql.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,17 +18,20 @@ public class MBWrite implements CommandProcess {
 		String mbd_sbjt = request.getParameter("mbd_sbjt");
 		String own_id = request.getParameter("own_id");
 		String mbd_cont = request.getParameter("mbd_cont");
-		String pageNum = request.getParameter("pageNum");
+		/* String pageNum = request.getParameter("pageNum"); */
+		Date mbd_str_dt = Date.valueOf(request.getParameter("mbd_str_dt"));
+		Date mbd_fn_dt = Date.valueOf(request.getParameter("mbd_fn_dt"));
 		
 //		mboard.setNum(num);
 		mboard.setMbd_sbjt(mbd_sbjt);
 		mboard.setOwn_id(own_id);
 		mboard.setMbd_cont(mbd_cont);
+		mboard.setMbd_str_dt(mbd_str_dt);
+		mboard.setMbd_fn_dt(mbd_fn_dt);
 		MBoardDao mbd = MBoardDao.getInstance();
 		int result = mbd.insert(mboard);
 		
 		request.setAttribute("result", result);
-		request.setAttribute("pageNum", pageNum);
 		return "mboard/mbWrite";
 	}
 
--- src/main/webapp/WEB-INF/views/mboard/mbWriteForm.jsp
+++ src/main/webapp/WEB-INF/views/mboard/mbWriteForm.jsp
@@ -11,14 +11,14 @@
 <body>
 <form action="mbWrite.do" method="post" name="frm">
 <input type="hidden" name="mbd_no" value="${mboard.mbd_no}">
-<input type="hidden" name="own_id" value="${member.own_id}">
+<%-- <input type="hidden" name="own_id" value="${member.own_id}"> --%>
 <input type="hidden" name="pageNum" value="${pageNum}">
 	<table>
 		<caption>산책 요청 작성</caption>
 		<!-- 작성자, 작성자 등록일은 히든으로 받아서 넘길 예정. -->
 		<tr>
 			<th>작성자</th>
-			<td>${mboard.own_id }</td>
+			<%-- <td>${mboard.own_id }</td> --%>
 			<td>
 				<input type="text" name="own_id" required="required">
 			</td>
