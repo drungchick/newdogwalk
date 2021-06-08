3b1a64c540ef84832f06d2bc0b77787545bc93eb
Junghuck Lee <jh_lee1117@naver.com>
2021-06-08 23:39:53 +0900

    예약 모델 관련 기본 모델만 저장

---
+++ src/main/java/dogwalk/model/Booking.java
@@ -0,0 +1,46 @@
+package dogwalk.model;
+
+public class Booking {
+	private String bk_no;
+	private String mbd_no;
+	private String bk_mc_chk;
+	private String bk_chk;
+	private String wkr_reg_no;
+	private String wkr_id;
+	public String getBk_no() {
+		return bk_no;
+	}
+	public void setBk_no(String bk_no) {
+		this.bk_no = bk_no;
+	}
+	public String getMbd_no() {
+		return mbd_no;
+	}
+	public void setMbd_no(String mbd_no) {
+		this.mbd_no = mbd_no;
+	}
+	public String getBk_mc_chk() {
+		return bk_mc_chk;
+	}
+	public void setBk_mc_chk(String bk_mc_chk) {
+		this.bk_mc_chk = bk_mc_chk;
+	}
+	public String getBk_chk() {
+		return bk_chk;
+	}
+	public void setBk_chk(String bk_chk) {
+		this.bk_chk = bk_chk;
+	}
+	public String getWkr_reg_no() {
+		return wkr_reg_no;
+	}
+	public void setWkr_reg_no(String wkr_reg_no) {
+		this.wkr_reg_no = wkr_reg_no;
+	}
+	public String getWkr_id() {
+		return wkr_id;
+	}
+	public void setWkr_id(String wkr_id) {
+		this.wkr_id = wkr_id;
+	}
+}
